ddf6816b606b1ebe34e3938ac00725e1b76983cf
Jeckfeldt <jeckfeldt123@gmail.com>
2021-06-08 14:02:01 -0700

    code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let waitlist = require('waitingArray')
+let tables = require('tableArray')
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -9,13 +11,26 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
-app.post('/Tables.js', (req,res)=>{
-  let reservation = {
-    customerName: ,
-    customerEmail: ,
-    customerID: ,
-    phoneNumber: 
-  }
+
+app.get('/api/tables', (req, res) => {
+  res.json(tables)
+})
+
+app.get('/api/waiting', (req, res) => {
+  res.json(waitlist)
+})
+
+// app.post('/Tables.js', (req,res)=>{
+//   let reservation = {
+//     customerName: ,
+//     customerEmail: ,
+//     customerID: ,
+//     phoneNumber: 
+//   }
+// })
+
+app.get('/api/tables', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.listen(3000)
 
