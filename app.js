ee6800872a9c43931f29e9d8437dc89653013442
Brandon Clark <clarkwbrandon2@gmail.com>
2021-06-08 11:53:45 -0700

    set up complete

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/table')
+app.listen(3000)
+
